e9da7902e4acd49cc441d3fb267dcf5475e02600
ken_yamamoto <ken.yamamoto@coincheck.com>
2017-12-29 11:33:37 +0900

    add transactions_pagination

--- src/coincheck/order.py
+++ src/coincheck/order.py
@@ -13,9 +13,13 @@ class Order(ServiceBase):
         defaults.update(params)
         params = defaults.copy()
         return self.coinCheck.request(ServiceBase.METHOD_DELETE, self.baseUrl + '/' + str(params['id']), params)
-    
+
     def opens(self, params = {}):
         return self.coinCheck.request(ServiceBase.METHOD_GET, self.baseUrl + '/opens', params)
-    
+
     def transactions(self, params = {}):
         return self.coinCheck.request(ServiceBase.METHOD_GET, self.baseUrl + '/transactions', params)
+
+    def transactions_pagination(self, params = {}):
+        return self.coinCheck.request(ServiceBase.METHOD_GET, self.baseUrl + '/transactions_pagination', params)
+
