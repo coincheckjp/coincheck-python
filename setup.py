08c81c41f0dd57c90efbe1056d42aec94914fe92
Max Levy <35595512+maxim-levy@users.noreply.github.com>
2019-05-17 15:22:41 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='coincheck-module',
+      version='1.1.0',
+      description='Coincheck API client.',
+      url='https://github.com/QuoineFinancial/coincheck-python',
+      install_requires=[
+      ],
+      packages=['src.coincheck', ]
+      )
